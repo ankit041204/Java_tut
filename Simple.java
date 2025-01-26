ca776436bd30c857803aaec2a2b696ad79a6be60
ankit041204 <ankits9121@gmail.com>
2025-01-26 17:02:58 +0530

    Java IF Statement

--- Simple.java
+++
@@ -1,9 +0,0 @@
-public class Simple{
-
-    public static void main (String args[]){
-        int a=10;
-        int b=34;
-        int c=a+b;
-        System.out.println(c);
-    }
-}
---
+++ ifStatement.java
@@ -0,0 +1,10 @@
+public class ifStatement{
+    
+    public static void main (String args[]){
+        int number =8;
+        if(number<0){
+            System.out.println("The number is negative");
+        }
+        System.out.println("Statement outside if block");
+    }
+}
