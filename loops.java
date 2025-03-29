6421cf52cc3486541a80566d3b41269c98003867
ankit041204 <ankits9121@gmail.com>
2025-03-29 21:05:12 +0530

    print name in java using loops 5 times

--- loops.java
+++ loops.java
@@ -1,8 +1,8 @@
 public class loops{
     public static void main (String args[]){
 
-        for (int i=0;i<11;i++){
-            System.out.println(i);
+        for (int i=0;i<5;i++){
+            System.out.println("My name is Ankit Singh");
         }
     }
 }
