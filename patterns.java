ee74069c3398ddd9ed8b8d5a9d5aeba95d22e9c7
ankit041204 <ankits9121@gmail.com>
2025-03-29 21:26:41 +0530

    Print the rectangle pattern in java using loops

---
+++ patterns.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class patterns{
+
+    public static void main(String args[]){
+
+        int n=4;
+        int m=6;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+
+                System.out.print("*");
+
+
+
+            }
+            System.out.println();
+        }
+    }
+}
