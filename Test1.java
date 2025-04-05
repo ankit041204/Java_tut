71e6965b87893ebc9b734f2542f1c02f6cb9de65
ankit041204 <ankits9121@gmail.com>
2025-04-05 10:40:32 +0530

    draw line method eith no parameters and no return type

---
+++ Test1.java
@@ -0,0 +1,12 @@
+public class Test1{
+    public static void main(String args[]){
+    
+        drawline();
+
+    }
+    public static void drawline(){
+        for (int i=0;i<10;i++){
+            System.out.print("_");
+        }
+    }
+}
