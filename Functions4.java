5022377d94ba9278398ab16ce23d3684969471e1
ankit041204 <ankits9121@gmail.com>
2025-05-24 14:55:01 +0530

    Function overloading using data types

---
+++ Functions4.java
@@ -0,0 +1,13 @@
+public class Functions4{
+    public static int sum(int a,int b){
+        return a+b;
+    }
+    public static float sum(float a,float b){
+        return a+b;
+    }
+    public static void main(String args[]){
+        System.out.println(sum(6,7));
+        System.out.println(sum(5.9f,9.8f));
+    }
+
+    }
