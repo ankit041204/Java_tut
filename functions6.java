7035e7a65d677b06bf18566fe28d551ed9568179
ankit041204 <ankits9121@gmail.com>
2025-05-25 14:43:50 +0530

    Print th prime no's in range using functions in java

---
+++ functions6.java
@@ -0,0 +1,24 @@
+public class functions6 {
+    // Method to print prime numbers up to n
+    public static void primesInRange(int n) {
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.println(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    // Method to check if a number is prime
+    public static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String args[]) {
+        primesInRange(20);
+    }
+}
