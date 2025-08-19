2dba5fb2815d1d380e47abe03744619cf29227fe
ankit041204 <ankits9121@gmail.com>
2025-08-19 23:26:14 +0530

    print number from 1 to n using taking user input

--- practice.java
+++ practice.java
@@ -1,10 +1,15 @@
 import java.util.Scanner;
 public class practice{
     public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        int range = sc.nextInt();
     int counter =1;
-    while (counter <= 10){
-        System.out.println(counter);
+    while (counter <= range){
+        System.out.print(counter+" ");
         counter ++;
     }
+    System.out.println();
     }
 }
