1b029601370dd3d99c88c7be4653ef73cb780107
ankit041204 <ankits9121@gmail.com>
2025-08-20 22:58:50 +0530

    reverse a number using loop

--- practice.java
+++ practice.java
@@ -1,9 +1,15 @@
 import java.util.Scanner;
 public class practice{
     public static void main(String args[]){
-        int i =1;
-        for (i=1;i<=10;i++){
-            System.out.println("Hello world");
+        int n=182368;
+        while(n>0){
+
+            int lastDigit = n%10;
+            System.out.print(lastDigit+" ");
+
+            n=n/10;
         }
+        System.out.println();
+  
     }
 }
