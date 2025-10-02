e47ef9b3f993cb4fd8bee860a0341dfb3c18fe47
ankit041204 <ankits9121@gmail.com>
2025-10-02 21:22:01 +0530

    correct single level inheritance in oop in java

--- SingleLevelInheritance.java
+++ SingleLevelInheritance.java
@@ -1,26 +1,19 @@
-class GrandParent {
-    void method1() {
-        System.out.println("GrandParent class");
-    }
-}
-
-class Parent extends GrandParent {
-    void method2() {
-        System.out.println("Parent class");
+class Parent {
+    void display() {
+        System.out.println("This is Parent class");
     }
 }
 
 class Child extends Parent {
-    void method3() {
-        System.out.println("Child class");
+    void show() {
+        System.out.println("This is Child class");
     }
 }
 
 public class SingleLevelInheritance {
     public static void main(String[] args) {
         Child obj = new Child();
-        obj.method1(); // From GrandParent
-        obj.method2(); // From Parent
-        obj.method3(); // From Child
+        obj.display(); // From Parent
+        obj.show();    // From Child
     }
 }
