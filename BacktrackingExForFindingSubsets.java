77ed05c48f7db7c0cec19bbadee5dc4861ad6465
ankit041204 <ankits9121@gmail.com>
2025-12-01 13:01:55 +0530

    Finding subsets using backtracking in java

---
+++ BacktrackingExForFindingSubsets.java
@@ -0,0 +1,21 @@
+public class BacktrackingExForFindingSubsets{
+    public static void findSubsets(String str,String ans,int i){
+
+        if (i ==str.length()){
+            if (ans.length()==0){
+                System.out.println("null");
+            }else{
+                System.out.println(ans);
+            }
+            return;
+        }
+        findSubsets(str,ans+str.charAt(i),i+1);
+
+        findSubsets(str,ans,i+1);
+    }
+    public static void main (String args[]){
+        String str = "abc";
+
+        findSubsets (str,"",0);
+    }
+}
