74ed3554ceb1839a25f7ec38c63eb2ffff54f78a
ankit041204 <ankits9121@gmail.com>
2026-02-02 23:46:40 +0530

    Remove a cycle in a linked list in java

---
+++ LinkedListRemoveIsCycle.java
@@ -0,0 +1,76 @@
+public class LinkedListRemoveIsCycle {
+
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+
+    // Method to check if cycle exists
+    public static boolean isCycle() {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Method to remove cycle
+    public static void removeCycle() {
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+
+        // Detect cycle
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                cycle = true;
+                break;
+            }
+        }
+
+        if (!cycle) {
+            return;
+        }
+
+        // Remove cycle
+        slow = head;
+        Node prev = null;
+
+        while (slow != fast) {
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        prev.next = null;
+    }
+
+    public static void main(String args[]) {
+        head = new Node(1);
+        Node temp = new Node(2);
+        head.next = temp;
+        head.next.next = new Node(3);
+        head.next.next.next = temp; // cycle created
+
+        System.out.println(isCycle()); // true
+        removeCycle();
+        System.out.println(isCycle()); // false
+    }
+}
