7f49e63b305f4225f9760eb7ff5c7e0ff4eca6c3
ankit041204 <ankits9121@gmail.com>
2026-02-11 21:01:27 +0530

    Reverse a Stack in java

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,1 +1,10 @@
-arr
+public static void pushAtBottom(Stack<Integer> s, int data){
+
+        if (s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
