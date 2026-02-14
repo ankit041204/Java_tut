d55b59b94b97c063aabed3914c228ee4ff770719
ankit041204 <ankits9121@gmail.com>
2026-02-14 22:08:31 +0530

    To identify the valid parenthesis in java using stacks

---
+++ StackValidparenthesis.java
@@ -0,0 +1,45 @@
+import java.util.Stack;
+
+public class StackValidparenthesis {
+
+    
+
+    public static boolean isValid(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            // If opening bracket, push to stack
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } 
+            // If closing bracket
+            else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                if ((stack.peek() == '(' && ch == ')') ||
+                    (stack.peek() == '{' && ch == '}') ||
+                    (stack.peek() == '[' && ch == ']')) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String str = "{[()]}";
+
+        if (isValid(str)) {
+            System.out.println("Valid Parenthesis");
+        } else {
+            System.out.println("Invalid Parenthesis");
+        }
+    }
+}
