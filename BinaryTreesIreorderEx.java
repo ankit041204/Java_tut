e9b782337a389aa18b73df75cec9148d3d38811c
ankit041204 <ankits9121@gmail.com>
2026-03-12 21:31:07 +0530

    Binary trees inorder example in java

---
+++ BinaryTreesIreorderEx.java
@@ -0,0 +1,39 @@
+public class BinaryTreesIreorderEx{
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);       // Left
+        System.out.print(root.data + " "); // Root
+        inorder(root.right);      // Right
+    }
+
+    public static void main(String[] args) {
+
+        Node root = new Node(1);
+
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        root.right.right = new Node(6);
+
+        inorder(root);
+    }
+}
