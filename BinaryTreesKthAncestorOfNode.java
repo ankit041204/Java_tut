a99c56ba77f0c8e98c78f97eb454a538ccad2696
ankit041204 <ankits9121@gmail.com>
2026-03-21 12:49:26 +0530

    To find the kth ancestor of a node in java

---
+++ BinaryTreesKthAncestorOfNode.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class BinaryTreesKthAncestorOfNode {
+
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int kthAncestor(Node root, int n, int k) {
+        if (root == null) {
+            return -1;
+        }
+
+        if (root.data == n) {
+            return 0;
+        }
+
+        int leftDist = kthAncestor(root.left, n, k);
+        int rightDist = kthAncestor(root.right, n, k);
+
+        if (leftDist == -1 && rightDist == -1) {
+            return -1;
+        }
+
+        int max = Math.max(leftDist, rightDist);
+
+        if (max + 1 == k) {
+            System.out.println("Kth Ancestor is: " + root.data);
+        }
+
+        return max + 1;
+    }
+
+    public static void main(String args[]) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        int n = 5; // target node
+        int k = 2; // kth ancestor
+
+        kthAncestor(root, n, k);
+    }
+}
