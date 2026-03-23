bddf33dacfe2eba63cacf10b21324b7d61e4d64f
ankit041204 <ankits9121@gmail.com>
2026-03-23 21:59:05 +0530

    To search a tree in a binary search tree in java

---
+++ BSTSearchBST.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+public class BSTSearchBST {
+
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // INSERT function
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            return new Node(val);
+        }
+
+        if (val < root.data) {
+            root.left = insert(root.left, val);
+        } else {
+            root.right = insert(root.right, val);
+        }
+
+        return root;
+    }
+
+    // INORDER Traversal (prints sorted order)
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    // SEARCH function (corrected)
+    public static boolean search(Node root, int key) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.data == key) {
+            return true;
+        }
+
+        if (root.data > key) {
+            return search(root.left, key);
+        } else {
+            return search(root.right, key);
+        }
+    }
+
+    public static void main(String args[]) {
+        int values[] = {5, 1, 3, 4, 2, 7};
+        Node root = null;
+
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
+        }
+
+        System.out.print("Inorder Traversal: ");
+        inorder(root);
+        System.out.println();
+
+        if (search(root, 9)) {
+            System.out.println("Key Found");
+        } else {
+            System.out.println("Key Not Found");
+        }
+    }
+}
