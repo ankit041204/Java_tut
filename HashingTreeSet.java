63e092cf2e8c2276b6521b1a17b8e53cd644f3c8
ankit041204 <ankits9121@gmail.com>
2026-06-12 17:42:03 +0530

    Tree Set Example using Hashing in java

---
+++ HashingTreeSet.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class HashingTreeSet {
+
+    public static void main (String args[]){
+        TreeSet<String> ts = new TreeSet<>();
+
+        ts.add("Delhi");
+        ts.add("Pune");
+        ts.add("Solapur");
+        ts.add("Patna");
+
+        System.out.println(ts);
+    }
+}
